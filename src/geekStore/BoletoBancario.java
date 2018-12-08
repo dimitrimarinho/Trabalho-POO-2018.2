8550f712b7955e714effb1dc979e581d2bbeb06c
Dimitri Marinho <dimitri.santana.marinho@gmail.com>
2018-12-08 19:26:45 -0300

    Project on Eclipse Photon

---
+++ src/geekStore/BoletoBancario.java
@@ -0,0 +1,20 @@
+package geekStore;
+
+public class BoletoBancario extends FormaPagamento {
+
+	private String Banco;
+
+	public BoletoBancario(String nome, int id, boolean ativo, String banco) {
+		super(nome, id, ativo);
+		Banco = banco;
+	}
+
+	public String getBanco() {
+		return Banco;
+	}
+
+	public void setBanco(String banco) {
+		Banco = banco;
+	}
+	
+}
